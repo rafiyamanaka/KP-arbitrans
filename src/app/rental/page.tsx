4ff35d414aa2ee07815db9b919447bf440242ecb
citraakumaladewii <2311103052@ittelkom-pwt.ac.id>
2025-08-17 22:00:40 +0700

    merge update beranda

--- src/app/rental/page.tsx
+++ src/app/rental/page.tsx
@@ -1,6 +1,7 @@
-import Header from "../_components/Header";
-import SearchbBar from "../_components/Searchbar";
+import Header from "../_components/header";
+import SearchbBar from "../_components/searchbar";
 import CarCard from "../_components/card";
+import Footer from "../_components/footer";
 const rentalPage = () => {
   return (
     <>
@@ -14,12 +15,87 @@ const rentalPage = () => {
         </p>
       </div>
       <SearchbBar />
-      <div className="container grid grid-cols-4 gap-6 mx-auto px-[100px]">
-        <CarCard />
-        <CarCard />
-        <CarCard />
-        <CarCard />
+      <div className="px-[100px] py-10">
+        {/* Grid 1 */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-[32px] justify-items-center mb-[32px]">
+          <CarCard
+            name="Toyota Avanza"
+            passengers={7}
+            luggage={2}
+            transmission="Manual"
+            fuel="Pertamax"
+            price="Rp350.000"
+          />
+          <CarCard
+            name="Honda Beat"
+            passengers={2}
+            luggage={0}
+            transmission="Matic"
+            fuel="Pertalite"
+            price="Rp80.000"
+          />
+          <CarCard
+            name="Honda Brio"
+            passengers={5}
+            luggage={1}
+            transmission="Matic"
+            fuel="Pertamax"
+            price="Rp300.000"
+          />
+          <CarCard
+            name="Honda Brio"
+            passengers={5}
+            luggage={1}
+            transmission="Matic"
+            fuel="Pertamax"
+            price="Rp300.000"
+          />
+        </div>
+
+        {/* Grid 2 */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-[32px] justify-items-center">
+          <CarCard
+            name="Toyota Avanza"
+            passengers={7}
+            luggage={2}
+            transmission="Manual"
+            fuel="Pertamax"
+            price="Rp350.000"
+          />
+          <CarCard
+            name="Honda Beat"
+            passengers={2}
+            luggage={0}
+            transmission="Matic"
+            fuel="Pertalite"
+            price="Rp80.000"
+          />
+          <CarCard
+            name="Honda Brio"
+            passengers={5}
+            luggage={1}
+            transmission="Matic"
+            fuel="Pertamax"
+            price="Rp300.000"
+          />
+          <CarCard
+            name="Honda Brio"
+            passengers={5}
+            luggage={1}
+            transmission="Matic"
+            fuel="Pertamax"
+            price="Rp300.000"
+          />
+        </div>
       </div>
+
+      {/* Button Muat Lebih Banyak */}
+      <div className="flex justify-center mt-[48px] mb-[48px]">
+        <button className="text-[#196FEB] bg-[#196FEB]/10 px-6 py-[15px] rounded font-semibold">
+          Muat Lebih Banyak
+        </button>
+      </div>
+      <Footer />
     </>
   );
 };
